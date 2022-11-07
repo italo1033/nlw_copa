4d63473fc23db85508f0615f362d7e814dd771c0
italo1033 <italogammy1033@gmail.com>
2022-11-07 02:01:00 -0300

    create route pools

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
 import {PrismaClient} from "@prisma/client"
+import ShortUniqueId from "short-unique-id"
+
+import {z} from "zod"
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -30,6 +33,30 @@ async function boostrap() {
         const pools = await prisma.pool.count()
         return {pools}
     })
+
+    
+
+    fastify.post("/pools", async(req, res)=>{
+
+        //o z da biblioteca zod vai impedir que a API coloque um valor nullo
+        const createPoolBody = z.object({
+            title:z.string(),
+        })
+
+        const {title} = createPoolBody.parse(req.body)
+
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase()
+        await prisma.pool.create({
+            data:{
+                title,
+                code,
+            }
+        })
+
+        //o codigo 201 significa criação
+        return  res.status(201).send({code})
+      })
 
     //informando a porta
     await fastify.listen({port:3333, /*host:'0.0.0.0' */})
